740333ca957b5269237f7db1eeb096cd90c80c8a
Konrad <konradbieniek@gmail.com>
2022-11-10 23:53:04 +0100

    changed api to pixabay

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-// Unsplash API
-const count = 10
-const apiKey = "WyA-OSpt68DYBZ1z-2Gf1x4nYqEn-vSKbAcNqkcSBOY"
+const imageContainer = document.getElementById("image-container")
+const loader = document.getElementById("loader")
 
-const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+let photosArr = []
+
+// create and add to DOM els for links and photos
+const displayPhotos = () => {
+	photosArr.forEach((photo) => {
+		const el = document.createElement("a")
+		el.setAttribute.href(photo)
+	})
+}
+
+// API
+// const count = 10
+const apiKey = "28643468-0bfd18c65d82c5dc5dca3f2bd"
+
+// const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+const url = `https://pixabay.com/api/?key=${apiKey}&category=backgrounds&image_type=photo&pretty=true`
 
 const getPhotos = async () => {
 	try {
 		const response = await fetch(url)
-		const data = await response.json
-		console.log(data)
+		photosArr = await response.json()
+		console.log(photosArr)
 	} catch (error) {}
 }
 getPhotos()
