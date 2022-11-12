59783de50784da5f0df8402acae261d0e095108d
Konrad <konradbieniek@gmail.com>
2022-11-12 01:07:54 +0100

    fixed slow loading

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ let photosArr = []
 let ready = false
 let imagesLoaded = 0
 let totalImages = 0
+let count = 5
 
 // check if al images are loaded
 const imageLoaded = () => {
-	console.log("image loaded")
+	// console.log("image loaded")
 	imagesLoaded++
 	if (imagesLoaded === totalImages) {
 		ready = true
 		loader.hidden = true
-		console.log(ready)
+		count = 200
+		// console.log(ready)
 	}
 }
 
@@ -28,7 +30,7 @@ const setAtributes = (element, attributes) => {
 const displayPhotos = () => {
 	imagesLoaded = 0
 	totalImages = photosArr.length
-	console.log(totalImages)
+	// console.log(totalImages)
 
 	photosArr.forEach((photo) => {
 		const el = document.createElement("a")
@@ -45,7 +47,7 @@ const displayPhotos = () => {
 		img.addEventListener("load", imageLoaded)
 		imageContainer.appendChild(el)
 		el.appendChild(img)
-		// console.log(photo)
+		// // console.log(photo)
 	})
 }
 
@@ -54,7 +56,7 @@ const displayPhotos = () => {
 const apiKey = "28643468-0bfd18c65d82c5dc5dca3f2bd"
 
 // const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
-const url = `https://pixabay.com/api/?key=${apiKey}&category=backgrounds&image_type=photo&pretty=true`
+const url = `https://pixabay.com/api/?key=${apiKey}&category=backgrounds&per_page=${count}&image_type=photo&pretty=true`
 
 const getPhotos = async () => {
 	try {
@@ -63,13 +65,13 @@ const getPhotos = async () => {
 		const photosObj = await response.json()
 		photosArr = photosObj.hits
 		displayPhotos()
-		console.log(photosArr)
+		// console.log(photosArr)
 	} catch (error) {}
 }
 
 // load more photos when scrooling is near bottom
 window.addEventListener("scroll", () => {
-	// console.log("scrolled")
+	// // console.log("scrolled")
 	if (
 		window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
 		ready
